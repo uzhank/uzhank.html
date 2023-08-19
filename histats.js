aa902a5301eaaf46c2352a52c358120c0eabb55a
uzhank <142578605+uzhank@users.noreply.github.com>
2023-08-20 00:36:20 +0800

    histats.js

--- histats.js
+++ histats.js
@@ -1,5 +1,4 @@
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,3786028,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -7,6 +6,4 @@ _Hasync.push(['Histats.track_hits', '']);
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3786028&101" alt="website free tracking" border="0"></a></noscript>
-<!-- Histats.com  END  -->
+})();
